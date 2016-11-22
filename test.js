d204003ca8b496e8624045c72252ff9a677e9014
Forrest Desjardins <desjardinsfg@gmail.com>
2016-11-22 13:59:06 -0900

    Test that airport diagrams are actually received in tests

--- test.js
+++ test.js
@@ -1,21 +1,32 @@
 /* global describe, it */
 
+const assert = require('chai').assert
 const airportDiagrams = require('./index')
 
 describe('airportDiagrams', () => {
-  it('should fetch airport diagrams for a single ICAO', (done) => {
-    airportDiagrams('KFDC').then(() => done())
+  it('should fetch airport diagrams for a single ICAO', () => {
+    return airportDiagrams('PANC').then(diagram => {
+      assert(diagram)
+    })
   })
 
-  it('should fetch aiport diagrams for an array of ICAOs', (done) => {
-    airportDiagrams(['KFDC', 'KZBW']).then(() => done())
+  it('should fetch aiport diagrams for an array of ICAOs', () => {
+    return airportDiagrams(['PANC', 'KJAX']).then(diagrams => {
+      assert(diagrams.length === 2)
+      diagrams.map(assert)
+    })
   })
 
-  it('should expose the list method', (done) => {
-    airportDiagrams.list('KFDC').then(() => done())
+  it('should expose the list method', () => {
+    return airportDiagrams.list('PANC').then(diagram => {
+      assert(diagram)
+    })
   })
 
-  it('should fetch airport diagrams for an array of ICAOs using the list method', (done) => {
-    airportDiagrams.list(['KFDC', 'KZBW']).then(() => done())
+  it('should fetch airport diagrams for an array of ICAOs using the list method', () => {
+    return airportDiagrams.list(['PANC', 'KJAX']).then(diagrams => {
+      assert(diagrams.length === 2)
+      diagrams.map(assert)
+    })
   })
 })
